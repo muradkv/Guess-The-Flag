a638f456575280bd1ee6658a79b3c2c0804b1a6b
murad <muradkv@gmail.com>
2019-04-16 23:01:35 +0300

    Initial commit

---
+++ Guess The Flag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guess The Flag
+//
+//  Created by murad on 16/04/2019.
+//  Copyright © 2019 murad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
