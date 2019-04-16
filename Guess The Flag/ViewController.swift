fb0c89e08c53ca3c790334ad3549529fbaab654b
murad <muradkv@gmail.com>
2019-04-16 23:42:28 +0300

    Show images on buttons

--- Guess The Flag/ViewController.swift
+++ Guess The Flag/ViewController.swift
@@ -13,11 +13,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        countries += ["us", "russia", "uk", "estonia", "nigeria", "poland", "monaco", "italy", "france", "germany", "spain", "england"]
+        
+        askQuestion()
     }
-
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 
 }
 
