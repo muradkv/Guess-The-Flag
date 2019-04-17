0f01aa5c49f212b90bd03024898c2232032567d1
murad <muradkv@gmail.com>
2019-04-17 22:44:42 +0300

    Check answer and use UIAlertController to show user score

--- Guess The Flag/ViewController.swift
+++ Guess The Flag/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         
         button1.setImage(UIImage(named: countries[0]), for: .normal)
@@ -45,5 +45,20 @@ class ViewController: UIViewController {
         title = countries[correctAnswer].uppercased()
     }
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .cancel, handler: askQuestion))
+        present(ac, animated: true)
+    }
 }
 
